5e42b0e4e92fe54029b5200c3bb3638d1fe7de43
Viktor <94440460+70Viktor@users.noreply.github.com>
2022-03-19 17:07:50 +0700

    adaptived "events"

--- js/script.js
+++ js/script.js
@@ -3,7 +3,17 @@ new Swiper('.swiper', {
 		nextEl: '.events-button-next',
 		prevEl: '.events-button-prev'
 	},
-	slidesPerView: 3,
-	spaceBetween: 30,
+	slidesPerView: 1,
+	spaceBetween: 10,
 	loop: true,
+	breakpoints: {
+		941: {
+			slidesPerView: 2,
+			spaceBetween: 20
+		},
+		1201: {
+			slidesPerView: 3,
+			spaceBetween: 30
+		}
+	}
 })
